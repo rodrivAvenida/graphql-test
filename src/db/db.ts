77a99595a1a87ab72de6a5a3319c8b96dbfcca14
rodrigo <rodrigo.velazco@avenida.com>
2021-11-25 09:55:09 -0300

    examples of relations

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,26 +1,36 @@
-import {Sequelize} from "sequelize";
+import { Sequelize } from "sequelize";
 import path from "path";
-import fs from 'fs';
+import fs from "fs";
 import sequelize from "sequelize";
 
-const connect = new Sequelize (`mysql://root:rodri7997jaja@localhost:3306/graph`,{logging:false})
+const connect = new Sequelize(
+  `mysql://root:rodri7997jaja@localhost:3306/graph`,
+  { logging: false }
+);
 
-const modelRoute = fs.readdirSync(path.join(__dirname,'/models'))
-.reduce((total:any[],model:any)=>{
-    let route = path.join(__dirname,'/models',model)
-    total.push(require(route))
-    return total
-},[])
+const modelRoute = fs
+  .readdirSync(path.join(__dirname, "/models"))
+  .reduce((total: any[], model: any) => {
+    let route = path.join(__dirname, "/models", model);
+    total.push(require(route));
+    return total;
+  }, []);
 
-modelRoute.forEach((model:any)=>model.default(connect))
+modelRoute.forEach((model: any) => model.default(connect));
 
-const {
-    user,
-    pet
-}=connect.models
+const { user, pet } = connect.models;
 
-user.hasOne(pet,{as:"owner",foreignKey:"id"})
+/***************************  UNO A UNO  ***********************************/
 
+// user.hasOne(pet, { foreignKey: "idDelQueLoCria" }); =====> el id del usuario se guardara dentro del registro de la mascota
+// user.belongsTo(pet, { foreignKey: "mascotilla" }); =====> el id de la mascota se guarda dentro del usuario
 
+/***************************  UNO A MUCHOS  ***********************************/
 
-export default {connect,models:connect.models}
+// user.hasMany(pet, { foreignKey: "idDelUsuario" });  ====>  el id del usuario se guardara en cada mascota que le pertenezca
+
+/*************************  MUCHOS A MUCHOS  ********************************/
+
+// user.belongsToMany(pet, { through: "user_pet" }); =====> through sera el nombre que tomara la tabla intermedia entre pets y users
+
+export default { connect, models: connect.models };
